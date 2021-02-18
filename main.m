9be73016b0f23658d4e0bce6eb13588b88339b72
nicofretti <nico.fretti@gmail.com>
2021-02-18 18:05:07 +0100

    Add loading bar and videos

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clc;
 
 
 display("Inizio programma...");
-video = VideoReader('shaky_car.avi');
+video = VideoReader('input\test_black.avi');
 nFrames = video.NumFrames;
 
 firstFrame = video.readFrame();
@@ -15,17 +15,22 @@ figure; anchor = imcrop(firstFrame);
 anchor = rgb2gray(anchor);
 [aR,aC] = size(anchor);
 close all;
-
+display(nFrames);
 % Inizio stabilizzazione dei frame
+
+fig = uifigure;
+d = uiprogressdlg(fig,'Title','Please Wait');
+drawnow
 for i = 1:nFrames-1
+    d.Value = i/nFrames;
     frame = video.readFrame();
     frame_gray = rgb2gray(frame);
     frames(:,:,:,i)=frame;
     corr_offset = CustomXcorr(anchor,frame_gray);
     new(:,:,:,i) = imtranslate(frame,[-(corr_offset(2)+round(C/2)),-(corr_offset(1)+round(R/2))],'FillValues',0);
 end
-
-output = VideoWriter('stabilized_video.mp4');
+close(d);
+output = VideoWriter('output.mp4');
 open(output);
 for i=1:size(frames,4)
     writeVideo(output,new(:,:,:,i));
